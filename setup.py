2b356d0a44e6ba494facbe29f1b7361f361c61ba
Philipp Geyer <philipp@geyer.co.uk>
2013-03-02 11:12:38 +0000

    First commit of Django example framework
    Should install and run on RedHat's OpenShift platform. Cannot find
    static admin data annoyingly

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='Django', 
+      version='1.0',
+      description='Django 1.5 example app',
+      author='Philipp Geyer', 
+      author_email='admin@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+
+      #  Uncomment one or more lines below in the install_requires section
+      #  for the specific client drivers/modules your application needs.
+      install_requires=[
+      'Django>=1.5',
+      ],
+     )
